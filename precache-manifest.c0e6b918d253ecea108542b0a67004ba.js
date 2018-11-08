793768c1dd4d2ec030a1dd5202ec519559907a67
Victor Bagu <victor@digitalpath.co.za>
2018-11-08 07:27:43 +0200

    Updates

---
+++ precache-manifest.c0e6b918d253ecea108542b0a67004ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "09a4519950c4454f72f9a99726fee629",
+    "url": "/infinite-image-scroller/static/media/previous.09a45199.svg"
+  },
+  {
+    "revision": "66b1d21cb5411531c9d7a2c3e8c626ac",
+    "url": "/infinite-image-scroller/static/media/next.66b1d21c.svg"
+  },
+  {
+    "revision": "88a5078c200e7eb031ad",
+    "url": "/infinite-image-scroller/static/js/runtime~main.88a5078c.js"
+  },
+  {
+    "revision": "76adc957979fbd2c3168",
+    "url": "/infinite-image-scroller/static/js/main.76adc957.chunk.js"
+  },
+  {
+    "revision": "059f22b01d78aed2f0e9",
+    "url": "/infinite-image-scroller/static/js/1.059f22b0.chunk.js"
+  },
+  {
+    "revision": "76adc957979fbd2c3168",
+    "url": "/infinite-image-scroller/static/css/main.597d824b.chunk.css"
+  },
+  {
+    "revision": "059f22b01d78aed2f0e9",
+    "url": "/infinite-image-scroller/static/css/1.b200e2f2.chunk.css"
+  },
+  {
+    "revision": "a62549ce9e325f23ae27776563e61bd3",
+    "url": "/infinite-image-scroller/index.html"
+  }
+];
